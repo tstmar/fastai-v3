96427687e73367bd1b877a7c7e21a8ec2798c420
tstmar <miaowen@gmail.com>
2019-06-30 15:51:57 +0800

    print to find proper threshold

--- app/server.py
+++ app/server.py
@@ -97,7 +97,9 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
+    prediction = learn.predict(img)
+    print prediction
+    class = prediction[0]
     return JSONResponse({'result': str(prediction)})
 
 
