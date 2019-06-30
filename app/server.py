a32aedbe5c4c972580e42f970af3434ed24065e3
tstmar <miaowen@gmail.com>
2019-06-30 15:57:42 +0800

    remove print

--- app/server.py
+++ app/server.py
@@ -98,7 +98,6 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)
-    print prediction
     class = prediction[0]
     return JSONResponse({'result': str(prediction)})
 
